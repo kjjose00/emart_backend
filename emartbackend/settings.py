1014b3ee45948a4a3a702cec7a3ee90136a7099a
jose john <kjjose00@gmail.com>
2023-12-25 17:14:48 +0530

    third

--- emartbackend/settings.py
+++ emartbackend/settings.py
@@ -57,6 +57,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -154,7 +155,7 @@ MEDIA_URL=f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 MEDIA_ROOT=""
 
 
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
